19abe541392d5f5014f80efb11dc7f8a79a50a22
Ilya Kogan <kogan@ohio.edu>
2016-05-07 13:00:05 -0400

    Working popup edit menu.

--- ui/scripts/controllers/home.js
+++ ui/scripts/controllers/home.js
@@ -11,6 +11,7 @@ angular.module('farnsworth')
 
         self.holding = null;
         self.editing = false;
+        self.moving = false;
         self.categories = {};
         self.categoryList = [];
         self.selectedCategory = null;
@@ -43,8 +44,13 @@ angular.module('farnsworth')
                 combo: 'right',
                 description: 'Select the tile to the right of the currently selected tile.',
                 callback: function() {
-                    if(self.selectedTileIndex < self.selectedCategory.tiles.length - 1) {
-                        self.selectedTile = self.selectedCategory.tiles[++self.selectedTileIndex];
+                    if(self.editing) {
+                    } else if(self.moving) {
+
+                    } else {
+                        if(self.selectedTileIndex < self.selectedCategory.tiles.length - 1) {
+                            self.selectedTile = self.selectedCategory.tiles[++self.selectedTileIndex];
+                        }
                     }
                 }
             });
@@ -53,8 +59,13 @@ angular.module('farnsworth')
                 combo: 'left',
                 description: 'Select the tile to the right of the currently selected tile.',
                 callback: function() {
-                    if(self.selectedTileIndex > 0) {
-                        self.selectedTile = self.selectedCategory.tiles[--self.selectedTileIndex];
+                    if(self.editing) {
+                    } else if(self.moving) {
+
+                    } else {
+                        if(self.selectedTileIndex > 0) {
+                            self.selectedTile = self.selectedCategory.tiles[--self.selectedTileIndex];
+                        }
                     }
                 }
             });
@@ -71,10 +82,14 @@ angular.module('farnsworth')
                 combo: 'down',
                 description: 'Select the tile below the currently selected tile.',
                 callback: function() {
-                    if(self.selectedCategoryIndex < self.categoryList.length - 1) {
-                        self.selectedCategory = self.categoryList[++self.selectedCategoryIndex];
+                    if(self.editing) {
 
-                        selectProperCategoryTile();
+                    } else if(self.moving) {
+                        if(self.selectedCategoryIndex < self.categoryList.length - 1) {
+                            self.selectedCategory = self.categoryList[++self.selectedCategoryIndex];
+
+                            selectProperCategoryTile();
+                        }
                     }
                 }
             });
@@ -83,47 +98,63 @@ angular.module('farnsworth')
                 combo: 'up',
                 description: 'Select the tile above the currently selected tile.',
                 callback: function() {
-                    if(self.selectedCategoryIndex > 0) {
-                        self.selectedCategory = self.categoryList[--self.selectedCategoryIndex];
+                    if(self.editing) {
+                    } else if(self.moving) {
+                        if(self.selectedCategoryIndex > 0) {
+                            self.selectedCategory = self.categoryList[--self.selectedCategoryIndex];
 
-                        selectProperCategoryTile();
+                            selectProperCategoryTile();
+                        }
                     }
                 }
             });
 
-            hotkeys.bindTo($scope).add({
-                combo: 'enter',
-                action: 'keydown',
-                description: 'Activate the selected tile. Hold to edit instead.',
-                callback: function() {
-                    if(!self.holding) {
-                        self.holding = $timeout(function() {
+            // angular-hotkeys doesn't support multiple actions on one hotkey
+            // so we need to do something a little interesting: rebind the hotkey
+            // as it's pressed.
+            var addEnterHotkey = function(action) {
+                hotkeys.del('enter');
+
+                hotkeys.bindTo($scope).add({
+                    combo: 'enter',
+                    action: action,
+                    description: 'Activate the selected tile. Hold to edit instead.',
+                    callback: function() {
+                        if(self.editing) {
                             self.holding = null;
-                            self.startEditing(self.selectedTile);
-                        }, self.constants.holdTime);
-                    }
-                }
-            });
+                        } else if(self.moving) {
+                        } else {
+                            if(!self.holding) {
+                                addEnterHotkey('keyup');
 
-            hotkeys.bindTo($scope).add({
-                combo: 'enter',
-                action: 'keyup',
-                callback: function() {
-                    if(self.holding) {
-                        $timeout.cancel(self.holding);
-                        self.activate(self.selectedTile);
-                        self.holding = null;
+                                self.holding = $timeout(function() {
+                                    self.startEditing(self.selectedTile);
+                                }, constants.holdTime);
+                            } else {
+                                addEnterHotkey('keydown');
+
+                                $timeout.cancel(self.holding);
+                                self.holding = null;
+                                self.activate(self.selectedTile);
+                            }
+                        }
                     }
-                }
-            });
+                });
+            }
+
+            addEnterHotkey('keydown');
         };
 
         self.addTile = function() {
             $location.path('/edit-tile');
         };
 
+        self.editTile = function() {
+            $location.path(`/edit-tile/${self.selectedCategory.name}/${self.selectedTileIndex}`);
+        };
+
         self.getTileStyle = function(tile) {
-            if(self.editing && self.selectedTile === tile) {
+            if(self.moving && self.selectedTile === tile) {
                 return {
                     'background-color': tinycolor(tile.backgroundColor).darken(constants.editingDarkness).toString(),
                     'color': tinycolor(tile.textColor).darken(constants.editingDarkness).toString()
@@ -144,12 +175,55 @@ angular.module('farnsworth')
             self.editing = true;
 
             $mdDialog.show({
-                controller: function() {
+                controller: function(hotkeys, $scope, $mdDialog) {
+                    //TODO: This has to match the order of the buttons in the
+                    // HTML. Rework at some point to make this less terrible.
+                    $scope.actions = [
+                        'arrange', 'edit', 'cancel', 'delete'
+                    ];
+                    $scope.action = 0;
 
+                    hotkeys.bindTo($scope).add({
+                        combo: 'right',
+                        description: 'Select the option right of the current option',
+                        callback: function() {
+                            if($scope.action < $scope.actions.length-1) {
+                                $scope.action++;
+                            }
+                        }
+                    });
+
+                    hotkeys.bindTo($scope).add({
+                        combo: 'left',
+                        description: 'Select the option left of the curren options.',
+                        callback: function() {
+                            if($scope.action > 0) {
+                                $scope.action--;
+                            }
+                        }
+                    });
+
+                    hotkeys.bindTo($scope).add({
+                        combo: 'enter',
+                        description: 'Activate the selected option.',
+                        callback: function() {
+                            if(!self.holding) {
+                                $mdDialog.hide($scope.actions[$scope.action]);
+                            }
+                        }
+                    })
                 },
                 templateUrl: 'views/dialogs/edit-tile-popup.html',
                 parent: angular.element(document.body),
                 clickOutsideToClose:true
+            }).then(function(result) {
+                switch(result) {
+                    case 'arrange': self.moving = true; break;
+                    case 'edit': self.editTile(); break;
+                    case 'delete': self.deleteTile(); break;
+                }
+            }).finally(function() {
+                self.editing = false;
             });
         };
     });
