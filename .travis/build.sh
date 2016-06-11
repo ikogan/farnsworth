e69e3b7edded571e73c70ddd9c6584b74d6a2659
Ilya Kogan <kogan@ohio.edu>
2016-06-11 19:19:27 -0400

    Move dependencies and build to external script

---
+++ .travis/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+    npm run dist:win64
+    npm run dist:win32
+    npm run dist:linux64
+    npm run dist:linux32
+elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+    npm run dist:darwin
+fi
---
+++ .travis/install-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+    sudo apt-add-repository ppa:ubuntu-wine/ppa -y
+    sudo apt-add-repository ppa:likemartinma/osslsigncode -y
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+
+    echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+
+    sudo apt-get update
+    sudo apt-get install build-essential icnsutils graphicsmagick xz-utils rpm wine1.8 mono-devel ca-certificates-mono osslsigncode gcc-multilib g++-multilib -y --no-install-recommends
+fi
