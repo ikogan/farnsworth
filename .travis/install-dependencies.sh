e0d137ba668e065bc78271618ba1fd8c1a3284d0
Ilya Kogan <kogan@ohio.edu>
2016-06-11 19:38:03 -0400

    Don't fail on apt-get warnings.

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -11,6 +11,6 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
     sudo dpkg --add-architecture i386
-    sudo apt-get update
+    sudo apt-get update || true
     sudo apt-get install build-essential icnsutils graphicsmagick xz-utils rpm wine1.8 mono-devel ca-certificates-mono osslsigncode gcc-multilib g++-multilib -y --no-install-recommends
 fi
