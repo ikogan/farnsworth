ab6ab2c1a28cca6e844fb590e601ded5a2864270
Ilya Kogan <kogan@ohio.edu>
2016-12-31 01:07:51 -0500

    Fix building both Windows versions.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,19 +3,22 @@ set -e
 
 echo "Building on ${TRAVIS_OS_NAME}..."
 
+rm -Rf dist/
 mkdir -p dist/release
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+if [[ "${TRAVIS_OS_NAME}" == "linux" || "${TRAVIS_OS_NAME}" == "all" ]]; then
     npm run dist:win64
-    mv -v dist/*.exe dist/release
+    FILENAME=$(basename "`ls dist/*.exe`" | sed 's/ /-/g' | tr "[:upper:]" "[:lower:]" | sed 's/farnsworth-launcher/farnsworth-launcher-x64/')
+    mv -v dist/*.exe dist/release/${FILENAME}
     npm run dist:win32
-    mv -v dist/*.exe dist/release
+    FILENAME=$(basename "`ls dist/*.exe`" | sed 's/ /-/g' | tr "[:upper:]" "[:lower:]" | sed 's/farnsworth-launcher/farnsworth-launcher-x86/')
+    mv -v dist/*.exe dist/release/${FILENAME}
     npm run dist:linux64
     npm run dist:linux32
     mv -v dist/*.rpm dist/release
     mv -v dist/*.deb dist/release
     mv -v dist/*.tar.gz dist/release
-elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+elif [[ "${TRAVIS_OS_NAME}" == "osx" || "${TRAVIS_OS_NAME}" == "all" ]]; then
     npm run dist:darwin
     mv -v dist/mac/*.dmg dist/release
 else
